0d2f8fa34000afe43ed117b74b96cda5deb67af5
yyman <13fmi39@ms.dendai.ac.jp>
2014-02-05 03:44:17 +0900

    gitに追加

---
+++ Picedge/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(396, 366);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(168, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(402, 384);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Image";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "画像読込";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Form1_Load);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 71);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 31);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "グレースケール化";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 108);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 31);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "ソーベルフィルタ算出";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 18);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(138, 30);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "ソーベルフィルタ横";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 54);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(138, 30);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "ソーベルフィルタ縦";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(6, 90);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(138, 31);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "ソーベルフィルタ統合";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Location = new System.Drawing.Point(12, 152);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(150, 130);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Result View";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button8);
+            this.groupBox3.Controls.Add(this.button7);
+            this.groupBox3.Location = new System.Drawing.Point(12, 288);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(150, 105);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Pencil";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(6, 55);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(138, 31);
+            this.button8.TabIndex = 6;
+            this.button8.Text = "エッジ描画";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(6, 18);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(138, 31);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "ベクトル化";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 408);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Picedge/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        Bitmap img, imgTrim, imgTrimW, imgTrimH, imgTrimWf, imgTrimHf, imgTrimT, imgP, imgE;
+        bool s = false, vf = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            DialogResult result = openDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                //fn = openDialog.SafeFileName;
+                img = new Bitmap(openDialog.FileName);
+                pictureBox1.Image = img;
+                imgTrim = img;
+            }
+            s = false;
+            vf = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int r, g, b, luminance;
+            if (s == false)
+            {
+                for (int i = 0; i < imgTrim.Width; i++)
+                    for (int j = 0; j < imgTrim.Height; j++)
+                    {
+                        r = (int)imgTrim.GetPixel(i, j).R;
+                        g = (int)imgTrim.GetPixel(i, j).G;
+                        b = (int)imgTrim.GetPixel(i, j).B;
+                        luminance = (int)(0.298912 * (double)r + 0.586611 * (double)g + 0.114478 * (double)b);
+                        Color col = Color.FromArgb(luminance, luminance, luminance);
+                        imgTrim.SetPixel(i, j, col);
+                    }
+                this.pictureBox1.Image = imgTrim;
+                imgTrimW = new Bitmap(imgTrim);
+                imgTrimH = new Bitmap(imgTrim);
+                imgTrimWf = new Bitmap(imgTrim);
+                imgTrimHf = new Bitmap(imgTrim);
+                imgTrimT = new Bitmap(imgTrim);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button2_Click(sender, e);
+            int nr = 1;
+            int[,] filter = new int[,]{
+                {-1, 0, 1},
+                {-2, 0, 2},
+                {-1, 0, 1}
+            };
+            int[,] fw = new int[imgTrim.Width, imgTrim.Height];
+            int[,] fh = new int[imgTrim.Width, imgTrim.Height];
+            if (s == false)
+            {
+                for (int i = 1; i < imgTrim.Width - 1; i++)
+                {
+                    for (int j = 1; j < imgTrim.Height - 1; j++)
+                    {
+                        int sumW = 0;
+                        int sumH = 0;
+                        int sumT = 0;
+                        for (int ii = 0; ii < 3; ii++)
+                        {
+                            for (int jj = 0; jj < 3; jj++)
+                            {
+                                sumW += imgTrim.GetPixel(i - 1 + ii, j - 1 + jj).R * filter[jj, ii];
+                                sumH += imgTrim.GetPixel(i - 1 + ii, j - 1 + jj).R * filter[ii, jj];
+                            }
+                        }
+                        
+                        Color col;
+                        if(sumW < 0)
+                            col = Color.FromArgb(255, 255, -sumW/10);//Colorに変換
+                        else
+                            col = Color.FromArgb(0, 0, sumW/10);
+                        imgTrimWf.SetPixel(i, j, col);
+                        if (sumH < 0)
+                            col = Color.FromArgb(255, 255, -sumH/10);//Colorに変換
+                        else
+                            col = Color.FromArgb(0, 0, sumH / 10);//Colorに変換
+                        fw[i, j] = sumW;
+                        fh[i, j] = sumH;
+                        imgTrimHf.SetPixel(i, j, col);
+                        sumW = Math.Abs(sumW);
+                        sumH = Math.Abs(sumH);
+                        sumT = (int)Math.Sqrt(Math.Pow(sumW, 2) + Math.Pow(sumH, 2));
+                        sumW = sumW / nr;
+                        sumW = (sumW >= 255) ? 255 : (sumW <= 0) ? 0 : sumW;
+                        sumH = (sumH >= 255) ? 255 : (sumH <= 0) ? 0 : sumH;
+                        sumT = (sumT >= 255) ? 255 : (sumT <= 0) ? 0 : sumT;
+                        col = Color.FromArgb(sumW, sumW, sumW);//Colorに変換
+                        imgTrimW.SetPixel(i, j, col);
+                        col = Color.FromArgb(sumH, sumH, sumH);//Colorに変換
+                        imgTrimH.SetPixel(i, j, col);
+                        col = Color.FromArgb(sumT, sumT, sumT);//Colorに変換
+                        imgTrimT.SetPixel(i, j, col);
+                    }
+                }
+            }
+            if (sender.Equals(button4) == true)
+                this.pictureBox1.Image = imgTrimW;
+            else if (sender.Equals(button5) == true)
+                this.pictureBox1.Image = imgTrimH;
+            else if (sender.Equals(button3) || sender.Equals(button6) == true)
+                this.pictureBox1.Image = imgTrimT;
+            s = true;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            double x, y, vx, vy, tx, ty, ttx, tty, v, tv;
+
+            if (s == true && vf == false)
+            {
+                imgP = new Bitmap(imgTrimT.Width, imgTrimT.Height);
+                imgE = new Bitmap(imgTrimT.Width, imgTrimT.Height);
+                Graphics g = Graphics.FromImage(imgP);//imgに描画する
+                Graphics ge = Graphics.FromImage(imgE);//imgに描画する
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                g.Clear(Color.White);
+                ge.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                ge.Clear(Color.White);
+                Pen p = new Pen(Color.FromArgb(50, Color.Black), 1);//ペンの色と太さ
+
+                vf = true;
+
+                for (int i = 1; i < imgTrimT.Width-1; i++)
+                {
+                    for (int j = 1; j < imgTrimT.Height-1; j++)
+                    {
+                        if (imgTrimT.GetPixel(i, j).R >= 100)
+                        {
+                            x = ((imgTrimWf.GetPixel(i, j).R > 0) ? -1 : 1) * imgTrimWf.GetPixel(i, j).B;
+                            y = ((imgTrimHf.GetPixel(i, j).R > 0) ? -1 : 1) * imgTrimHf.GetPixel(i, j).B;
+                            tx = x * Math.Cos(Math.PI / 2) - y * Math.Sin(Math.PI / 2);
+                            ty = x * Math.Sin(Math.PI / 2) + y * Math.Cos(Math.PI / 2);
+                            v = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+                            tv = Math.Sqrt(Math.Pow(tx, 2) + Math.Pow(ty, 2));
+                            vx = i + x / v * 20;
+                            vy = j + y / v * 20;
+                            ttx = i + tx / tv * 10;
+                            tty = j + ty / tv * 10;
+                            g.DrawLine(p, new Point(i, j), new Point((int)vx, (int)vy));
+                            //g.DrawLine(p, new Point(i, j), new Point(i+(int)vvw, j+(int)vvh));
+                            ge.DrawLine(p, new Point(i - (int)(tx / tv * 10), j - (int)(ty / tv * 10)), new Point((int)ttx, (int)tty));
+
+                        }
+                    }
+                }
+            }
+            this.pictureBox1.Image = imgP;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (vf)
+                this.pictureBox1.Image = imgE;
+        }
+
+    }
+}
